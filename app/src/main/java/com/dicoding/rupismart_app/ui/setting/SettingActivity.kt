8d5791c73516e79e323777b9c088721ecde5c83a
fauzywan <iwanfauzay46@gmail.com>
2024-11-29 14:43:31 +0700

    slicing setting activity

--- app/src/main/java/com/dicoding/rupismart_app/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/dicoding/rupismart_app/ui/setting/SettingActivity.kt
@@ -1,8 +1,10 @@
 package com.dicoding.rupismart_app.ui.setting
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.rupismart_app.databinding.ActivitySettingBinding
+import java.util.Locale
 
 class SettingActivity : AppCompatActivity() {
 
@@ -14,7 +16,21 @@ class SettingActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
-
+        setupView()
         binding.mainAppBar.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setupView() {
+
+        val locale = Locale.getDefault()
+        binding.tvLangSetting.text = "${locale.displayLanguage} ${ifSameLangCountry(locale)}"
+    }
+
+    private fun ifSameLangCountry(locale: Locale): String {
+        if(locale.displayCountry!=locale.displayLanguage){
+            return "(${locale.displayCountry})"
+        }
+        return ""
     }
 }
